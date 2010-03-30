81c6f241dc4e9fbef2bb754b8e243fd953c2d78f
eric <eric>
2010-03-30 08:04:31 +0000

    add usage

--- create_application.bash
+++ create_application.bash
@@ -1,34 +1,41 @@
 #!/bin/bash
 
+usage="$0 -a <appname> -m <modulename> -i <output directory>"
+
 appname=''
 module=''
 while getopts ":a:m:o:" opt; do
   case $opt in
     a)
-      echo "-a was triggered, Parameter: $OPTARG" >&2
+      echo "Application name: $OPTARG" >&2
       appname=$OPTARG
       ;;
     m)
-      echo "-m was triggered, Parameter: $OPTARG" >&2
+      echo "Module name: $OPTARG" >&2
       module=$OPTARG
       ;;
     o)
-      echo "-o was triggered, Parameter: $OPTARG" >&2
+      echo "Output directory: $OPTARG" >&2
       dir=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      echo $usage
       exit 1
       ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2
+      echo $usage
       exit 1
       ;;
   esac
 done
 
-echo $appname $module
 
+if [ -z "$appname" -o -z "$module" -o -z "$dir" ]; then
+     echo $usage
+      exit 1
+fi
 
 mkdir -p $dir
 if [ $? != 0 ]; then
@@ -36,13 +43,15 @@ if [ $? != 0 ]; then
   exit 1;
 fi
 cp -r * $dir
-pushd $dir
+pushd $dir >/dev/null
 APPNAME=`echo $appname  | tr [a-z] [A-Z]`
 
 mv TEMPLATE.app ${APPNAME}.app
 mv TEMPLATE_init.php.in ${APPNAME}_init.php.in
 mv TEMPLATE_en.po ${APPNAME}_en.po
 mv TEMPLATE_fr.po ${APPNAME}_fr.po
+rm -fr CVS
+rm -f create_application.bash
 
 sed -i -e"s/freedom-template/$module/" configure.in
 sed -i -e"s/TEMPLATE/$APPNAME/" configure.in
